1301b7955ffe1a19e7ed6b32484dbac91e861f2d
ernestsmotlak <ernestsmotlak@gmail.com>
2024-05-09 14:45:57 +0200

    Created war-database.db.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- backend/App.js
+++ backend/App.js
@@ -1,15 +1,23 @@
-// Import required modules
 const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors'); // Import the cors middleware
 
-// Create an Express application
 const app = express();
-const port = 3010;
+const port = 3000;
 
-// Define a sample route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors()); // Use cors middleware to enable CORS
+
+// Connect to SQLite database
+const db = new sqlite3.Database('sqlDataBase/Baza.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error('Error connecting to database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
 });
 
+app.use(bodyParser.json());
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
