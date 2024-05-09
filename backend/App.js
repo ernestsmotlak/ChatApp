880fef036f2c9731a5be02f93e867272f1470394
ernestsmotlak <ernestsmotlak@gmail.com>
2024-05-09 14:49:31 +0200

    Added a welcome message for the api.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- backend/App.js
+++ backend/App.js
@@ -4,12 +4,12 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors'); // Import the cors middleware
 
 const app = express();
-const port = 3000;
+const port = 3010;
 
 app.use(cors()); // Use cors middleware to enable CORS
 
 // Connect to SQLite database
-const db = new sqlite3.Database('sqlDataBase/Baza.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('War-database.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error('Error connecting to database:', err.message);
     } else {
@@ -17,8 +17,12 @@ const db = new sqlite3.Database('sqlDataBase/Baza.db', sqlite3.OPEN_READWRITE, (
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the War Card Game API!');
+});
+
 app.use(bodyParser.json());
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
