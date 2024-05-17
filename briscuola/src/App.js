28ee09e57432f556a0eb6f285ecc77f452995217
ernestsmotlak <ernestsmotlak@gmail.com>
2024-05-17 10:51:37 +0200

    Createad and edited LandinPage component.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- briscuola/src/App.js
+++ briscuola/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Login_SignUp/Register';
 import Login from './Login_SignUp/Login';
 import NotFound from './Components/NotFound';
+import LandingPage from './Components/LandingPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         App here.
         <Routes>
+          <Route path='/' element={<LandingPage />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='*' element={<NotFound />} />
---
+++ briscuola/src/Components/LandingPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const LandingPage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className='w-50' style={{margin: '0 auto'}}>
+            <h2 className='mt-5'>LandingPage</h2>
+            <div class="card text-center">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                <div class="card-footer text-body-secondary">
+                    2 days ago
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
